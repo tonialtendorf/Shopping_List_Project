c4f9ad3b0da940acfccb861647786b493671f8e6
Taryn Ferraro <tferraro517@gmail.com>
2023-03-21 21:55:06 -0400

    api routes created

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,6 @@
-// Taryn
+const router = require("express").Router();
+const productRoutes = require("./productRoutes");
+const userRoutes = require("./userRoutes");
+router.use("/product", productRoutes);
+router.use("/user", userRoutes);
+module.exports = router;
