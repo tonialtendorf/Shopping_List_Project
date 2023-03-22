21cba914a8d8dc3110ff44da8ccd9ff5dbdf1399
Taryn Ferraro <tferraro517@gmail.com>
2023-03-21 22:22:34 -0400

    added helpers.js

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,9 +1,8 @@
-//Jessie
-const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Project } = require("../models");
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const userData = require("./userData.json");
+const projectData = require("./projectData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
