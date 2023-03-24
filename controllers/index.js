9388e7344122a32dc71470b4116630326ecb6d62
tonialtendorf <tvohnoutka05@gmail.com>
2023-03-23 22:09:26 -0500

    routes tested

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
-
+const main = require("./main")
 
 router.use("/api", apiRoutes);
+router.use("/home", main)
 
 module.exports = router;
--- controllers/main.js
+++ controllers/main.js
@@ -1,61 +1,66 @@
 const router = require("express").Router();
 const { product } = require("../models");
 
-// GET all galleries for homepage - requesting for homepage
 router.get("/", async (req, res) => {
-  try {
-    const dbProductData = await product.findAll({
-      include: [
-        {
-          model: Product,
-          attributes: ["name", "category", "price"],
-        },
-      ],
+  res.render("layouts/main")
+  //try catch next.
     });
 
-    const products = dbProductData.map((product) =>
-      product.get({ plain: true })
-    );
+// // GET all galleries for homepage - requesting for homepage
+// router.get("/", async (req, res) => {
+//   try {
+//     const dbProductData = await product.findAll({
+//       include: [
+//         {
+//           model: Product,
+//           attributes: ["name", "category", "price"],
+//         },
+//       ],
+//     });
 
-    res.render("homepage", {
-      products,
-      loggedIn: req.session.logged,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+//     const products = dbProductData.map((product) =>
+//       product.get({ plain: true })
+//     );
 
-router.get("/product/:id", async (req, res) => {
-  try {
-    const dbProductData = await Product.findByPk(req.params.id, {
-      include: [
-        {
-          model: Product,
-          attributes: ["id", "name", "category", "price"],
-        },
-      ],
-    });
+//     res.render("homepage", {
+//       products,
+//       loggedIn: req.session.logged,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
-    const product = dbProductData.get({ plain: true });
-    // TODO: Send over the 'loggedIn' session variable to the 'gallery' template
-    res.render("product", { product });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+// router.get("/product/:id", async (req, res) => {
+//   try {
+//     const dbProductData = await Product.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: Product,
+//           attributes: ["id", "name", "category", "price"],
+//         },
+//       ],
+//     });
 
-// Login route
-router.get("/login", (req, res) => {
-  // If the user is already logged in, redirect to the homepage
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-  // Otherwise, render the 'login' template
-  res.render("login");
-});
+//     const product = dbProductData.get({ plain: true });
+//     // TODO: Send over the 'loggedIn' session variable to the 'gallery' template
+//     res.render("product", { product });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+// // Login route
+// router.get("/login", (req, res) => {
+//   // If the user is already logged in, redirect to the homepage
+//   if (req.session.loggedIn) {
+//     res.redirect("/");
+//     return;
+//   }
+//   // Otherwise, render the 'login' template
+//   res.render("login");
+// });
 
 module.exports = router;
