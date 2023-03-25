e3ed365fd9c04e8c83788deaf0d55d58782d7deb
tonialtendorf <tvohnoutka05@gmail.com>
2023-03-25 10:30:27 -0500

    tested shopping route

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,29 +1,36 @@
 const router = require('express').Router();
 const { Product } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll();
+    res.json(productData);
+  } catch (err) {res.status(500).json(err)}
+})
+
 router.post('/', async (req, res) => {
   try {
-    const newProduct = await newProduct.create({
-      ...req.body,
-    });
-    res.status(200).json(newProduct);
+    const productData = await productData.create(req.body);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
-router.delete('/:id', async (req, res) => {
-  try {
-    const productData = await Product.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!productData) {
-      res.status(404).json({ message: 'No product found with this id!' });
-      return;
-    }
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+
 module.exports = router;
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const productData = await Product.destroy({
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+//     if (!productData) {
+//       res.status(404).json({ message: 'No product found with this id!' });
+//       return;
+//     }
+//     res.status(200).json(productData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const main = require("./main")
+const productRoutes = require('./api/productRoutes')
+const userRoutes = require('./api/userRoutes')
 
 router.use("/api", apiRoutes);
+router.use('/shopping', productRoutes);
+router.use('/user', userRoutes)
 router.use("/home", main)
 
 module.exports = router;
