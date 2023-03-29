50757380ddb843b9804df6a40f1ea6580ba349a2
tonialtendorf <tvohnoutka05@gmail.com>
2023-03-29 15:30:19 -0500

    updated open text field functionality

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -50,7 +50,7 @@ itembtn.addEventListener('click', async function() {
                 }
             })
             if (res.ok) {
-                res.render('shopping', { name })
+                // res.render('shopping', { name })
                 // document.location.reload()
             } else {
                 alert('Something Went Wrong')
@@ -59,9 +59,116 @@ itembtn.addEventListener('click', async function() {
     }
 })
 
+//event listener for search button
+const btn = document.querySelector('.btn');
+const box = document.querySelector('.box');
+const history = document.querySelector('.history');
 
+btn.addEventListener('click', function() {
+})
 
-// console.log($("#itemList").find(':selected').attr('data-category'));
-// const price = $("#itemList").find(':selected').attr('data-price');
-// const itemList = document.getElementById('itemList');
-// const category = $("#itemList").find(':selected').attr('data-category');
+//localStorage
+const form = document.querySelector("#form");
+const searchBar = document.querySelector("#cityInput");
+const submitButton = document.querySelector(".btn");
+const deleteButton = document.querySelector("#delete");
+const ul = document.querySelector("#ul");
+let recentSearches;
+
+if (localStorage.recentSearches && localStorage.recentSearches != "") {
+  recentSearches = JSON.parse(localStorage.recentSearches);
+} else {
+  recentSearches = [];
+}
+//   const makeListItem = (text, parent) => {
+//     let listItem = document.createElement("li");
+//     listItem.textContent = text;
+//     listItem.className = "list-group-item";
+//     let checkSymbol = document.createElement("span");
+//     checkSymbol.innerHTML = "&#10004;";
+//     checkSymbol.className = "check-symbol";
+//     let crossSymbol = document.createElement("span");
+//     crossSymbol.innerHTML = "&#10006;";
+//     crossSymbol.className = "cross-symbol";
+//     crossSymbol.addEventListener("click", function() {
+//       // Perform desired action when cross symbol is clicked
+//       listItem.remove(); // Remove the list item when cross symbol is clicked
+//     });
+//     listItem.appendChild(checkSymbol);
+//     listItem.appendChild(crossSymbol);
+//     parent.appendChild(listItem);
+//   };
+
+  const makeListItem = (text, parent) => {
+    let listItem = document.createElement("li");
+    listItem.textContent = text;
+    listItem.className = "list-group-item";
+    let checkSymbol = document.createElement("span");
+    checkSymbol.innerHTML = "&#10004;";
+    checkSymbol.className = "check-symbol";
+    let crossSymbol = document.createElement("span");
+    crossSymbol.innerHTML = "&#10006;";
+    crossSymbol.className = "cross-symbol";
+    crossSymbol.addEventListener("click", function() {
+    
+      listItem.remove(); 
+    });
+    checkSymbol.addEventListener("click", function() {
+    
+      this.setAttribute('checked', !this.getAttribute('checked'));
+    });
+    listItem.appendChild(checkSymbol);
+    listItem.appendChild(crossSymbol);
+    parent.appendChild(listItem);
+  };
+
+recentSearches.forEach((element) => {
+  makeListItem(element, ul);
+});
+
+const isDuplicateValue = (arr, text) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == text) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+form.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (
+    searchBar.value == "" ||
+    isDuplicateValue(recentSearches, searchBar.value)
+  ) {
+    return;
+  } else {
+    recentSearches.push(searchBar.value);
+    makeListItem(searchBar.value, ul);
+    localStorage.recentSearches = JSON.stringify(recentSearches);
+    searchBar.value = "";
+  }
+});
+
+deleteButton.addEventListener("click", () => {
+  localStorage.clear();
+  recentSearches = [];
+  searchBar.value = "";
+
+  let arr = document.querySelectorAll("li");
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].remove();
+  }
+});
+
+//add event listener to search history item
+ul.addEventListener("click", function (event) {
+  event.preventDefault();
+  cityInput.style.display.inline;
+});
+
+document.getElementById("delete").addEventListener("click", restore, false);
+function restore() {
+  document.getElementById("delete").value = localStorage.getItem("delete");
+}
