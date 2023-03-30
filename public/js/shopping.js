77b20777e4127f7b6195a34523f7f743708534b8
tonialtendorf <tvohnoutka05@gmail.com>
2023-03-30 09:34:06 -0500

    dropdown selections

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -12,15 +12,14 @@ itembtn.addEventListener("click", async function () {
       },
     });
     if (res.ok) {
-      if (response.ok) {
-        const newProduct = await response.json(); // parse the response JSON
-        const productTemplate = Handlebars.compile(
-          document.querySelector("#product-template").innerHTML
-        );
-        const productHtml = productTemplate(newProduct); // render the new item using Handlebars
-        document
-          .querySelector(".product-list")
-          .insertAdjacentHTML("beforeend", productHtml); // append the new i
+        // const newProduct = await res.json(); // parse the response JSON
+        // const productTemplate = Handlebars.compile(
+        //   document.querySelector("#product-template").innerHTML
+        // );
+        // const productHtml = productTemplate(newProduct); // render the new item using Handlebars
+        // document
+        //   .querySelector(".product-list")
+        //   .insertAdjacentHTML("beforeend", productHtml); 
         // let listItem = document.createElement("li");
         // listItem.textContent = text;
         // listItem.className = "list-group-item";
@@ -43,6 +42,60 @@ itembtn.addEventListener("click", async function () {
         alert("Something Went Wrong");
       }
     }
+  }
+);
+
+itembtn.addEventListener("click", function () {});
+
+//localStorage for dropdown
+const dropdown = document.querySelector("#dropdown");
+const itemList = document.querySelector("#itemList");
+const addBtn = document.querySelector("#item-btn");
+let dropdownSelect;
+
+if (localStorage.dropdownSelect && localStorage.dropdownSelect != "") {
+  dropdownSelect = JSON.parse(localStorage.dropdownSelect);
+} else {
+  dropdownSelect = [];
+}
+
+const makeDropdownItem = (text, parent) => {
+  let listItem = document.createElement("li");
+  listItem.textContent = text;
+  listItem.className = "list-group-item";
+  let checkSymbol = document.createElement("span");
+  checkSymbol.innerHTML = "&#10004;";
+  checkSymbol.className = "check-symbol";
+  let crossSymbol = document.createElement("span");
+  crossSymbol.innerHTML = "&#10006;";
+  crossSymbol.className = "cross-symbol";
+  crossSymbol.addEventListener("click", function () {
+    listItem.remove();
+  });
+  checkSymbol.addEventListener("click", function () {
+    this.setAttribute("checked", !this.getAttribute("checked"));
+  });
+  listItem.appendChild(checkSymbol);
+  listItem.appendChild(crossSymbol);
+  parent.appendChild(listItem);
+};
+
+dropdownSelect.forEach((element) => {
+  makeDropdownItem(element, ul);
+});
+
+dropdown.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (
+    itemList.value == "" ||
+    isDuplicateValue(dropdownSelect, itemList.value)
+  ) {
+    return;
+  } else {
+    dropdownSelect.push(itemList.value);
+    makeDropdownItem(itemList.value, ul);
+    localStorage.dropdownSelect = JSON.stringify(dropdownSelect);
+    itemList.value = "";
   }
 });
 
